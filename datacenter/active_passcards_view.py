b14e89a1a4f772c6a64c5d3f6ea652c9b7f05bea
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-06-08 21:36:13 +0300

    Update active_passcards_view.py

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -1,9 +1,6 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from datetime import timedelta
-from datacenter.models import get_duration, format_duration, is_visit_long
-from django.utils.timezone import localtime, now
 
 
 def active_passcards_view(request):
