db4768a1c2939938070989105bcf32a45f4fa6af
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-06-08 21:35:56 +0300

    Удалил лишнюю часть кода

--- datacenter/active_passcards_view.py
+++ datacenter/active_passcards_view.py
@@ -7,34 +7,6 @@ from django.utils.timezone import localtime, now
 
 
 def active_passcards_view(request):
-    unclosed_visits = Visit.objects.filter(leaved_at__isnull=True)
-
-    for visit in unclosed_visits:
-        duration = get_duration(visit)
-        formatted_duration = format_duration(duration)
-
-        print(f'Зашёл в хранилище, время по Москве:\n{localtime(visit.entered_at)}')
-        print(f'Находится в хранилище:\n{formatted_duration}')
-
-    for visit in unclosed_visits:
-        print(visit.passcard.owner_name)
-
-    threshold_minutes = 10
-
-    suspicious_visits = []
-
-    all_visits = Visit.objects.all()
-
-    for visit in all_visits:
-        if is_visit_long(visit, minutes=threshold_minutes):
-            suspicious_visits.append(visit)
-
-    print(f'Визиты дольше {threshold_minutes} мин: {suspicious_visits}')
-
-    example_passcard = Passcard.objects.all()[0]
-    example_visits = Visit.objects.filter(passcard=example_passcard)
-    print(example_visits)
-
     active_passcards = Passcard.objects.filter(is_active=True)
 
     context = {
